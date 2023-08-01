9622acebf15352ce73e45bfcf7f647c82df63f3c
RussOakham <73036915+RussOakham@users.noreply.github.com>
2023-08-01 22:33:41 +0100

    feature: HeroBannerLarge - refactored to make into reusable component

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -1,39 +1,17 @@
-import Image from 'next/image'
+import HeroBannerLarge from '@/components/layout/hero-banner-large'
 
-const Homepage = () => {
+const Homepage: React.FC = () => {
 	return (
-		<div className="h-[70vh] w-full">
-			<div className="relative w-screen" style={{ height: 'inherit ' }}>
-				<div className="absolute -z-10 w-full" style={{ height: 'inherit ' }}>
-					<Image
-						src="/images/heroes/steeleford-green-lotus.webp"
-						alt="Steeleford Green Lotus"
-						height={2000}
-						width={2000}
-						priority
-						className="h-full object-cover object-center"
-					/>
-				</div>
-
-				<div className="relative flex h-full flex-col items-center justify-center">
-					<div className="flex max-w-lg flex-col items-center">
-						<Image
-							src="/images/logos/Steeleford-Prime-Logo-Red.webp"
-							alt="Steeleford Supercar Show"
-							width={400}
-							height={300}
-						/>
-						<div className="text-center font-bold text-white">
-							<h1 className="sr-only hidden">Steeleford Supercar Show</h1>
-							<h3 className="mb-4 text-4xl md:text-7xl">2nd July 2023.</h3>
-							<p className="text-sm md:text-2xl">
-								Thank you all so much for coming!
-							</p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+		<HeroBannerLarge
+			imageSrc="/images/heroes/steeleford-green-lotus.webp"
+			imageAlt="Steeleford Green Lotus"
+			logoSrc="/images/logos/Steeleford-Prime-Logo-Red.webp"
+			logoAlt="Steeleford Supercar Show"
+		>
+			<h1 className="sr-only hidden">Steeleford Supercar Show</h1>
+			<h3 className="mb-4 text-4xl md:text-7xl">2nd July 2023.</h3>
+			<p className="text-sm md:text-2xl">Thank you all so much for coming!</p>
+		</HeroBannerLarge>
 	)
 }
 
---
+++ src/components/layout/hero-banner-large.tsx
@@ -0,0 +1,51 @@
+import Image from 'next/image'
+
+interface HeroBannerLargeProps {
+	imageSrc: string
+	imageAlt: string
+	logoSrc?: string
+	logoAlt?: string
+	children?: React.ReactNode
+}
+
+const HeroBannerLarge = ({
+	children,
+	imageAlt,
+	imageSrc,
+	logoAlt,
+	logoSrc,
+}: HeroBannerLargeProps) => {
+	return (
+		<div className="h-[70vh] w-full">
+			<div className="relative w-screen" style={{ height: 'inherit ' }}>
+				<div className="absolute -z-10 w-full" style={{ height: 'inherit ' }}>
+					<Image
+						src={imageSrc}
+						alt={imageAlt}
+						height={2000}
+						width={2000}
+						priority
+						className="h-full object-cover object-center"
+					/>
+				</div>
+
+				<div className="relative flex h-full flex-col items-center justify-center">
+					<div className="flex max-w-lg flex-col items-center">
+						{logoSrc ? (
+							<Image
+								src={logoSrc}
+								alt={logoAlt ?? ''}
+								width={400}
+								height={300}
+							/>
+						) : null}
+
+						<div className="text-center font-bold text-white">{children}</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default HeroBannerLarge
